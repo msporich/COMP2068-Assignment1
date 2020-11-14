0ec672527a6833457e5de89dfb54488ff210351a
msporich <marksporich@gmail.com>
2020-11-14 16:44:27 -0500

    added all required pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'IT Company Portfolio' });
+});
+
+/* GET About Page */
+router.get('/about', function (req, res, next) {
+    res.render('about');
+});
+
+/* GET Projects Page */
+router.get('/projects', function (req, res, next) {
+    res.render('projects');
+});
+
+/* GET Services Page */
+router.get('/services', function (req, res, next) {
+    res.render('services');
+});
+
+/* GET Contact Page */
+router.get('/contact', function (req, res, next) {
+    res.render('contact');
 });
 
 module.exports = router;
